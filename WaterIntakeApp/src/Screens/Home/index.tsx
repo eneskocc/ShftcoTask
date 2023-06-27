1240d8bbdef0d0a138a7d31d4f3d009e35e41246
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-06-27 12:23:18 +0300

    start

---
+++ WaterIntakeApp/src/Screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React,{useState} from 'react'
+
+const Home = () => {
+    const [first, setfirst] = useState("")
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ WaterIntakeApp/src/config/index.js
@@ -0,0 +1,1 @@
+export const apiUrl="https://645ce732e01ac6105896bbce.mockapi.io/";
